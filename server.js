02eea3255d311d571c1487416038ece3e34e2bc0
Basem98 <basem.mostafa98@hotmail.com>
2020-09-03 17:00:33 +0200

    feat: Start up an express server to act as a back end

--- server.js
+++ server.js
@@ -1,42 +1,37 @@
-/**
- * Setup empty JS object to act as endpoint for all routes
- */
+/* Setup empty JS object to act as endpoint for all routes */
 const projectData = {};
 
-/**
- * Require Express to run server and routes
- */
+/* Require Express to run server and routes */
 const express = require('express');
 
-/**
- * Start up an instance of app
- */
+/* Start up an instance of app */
 const app = express();
 
-/**
- * The port that the server will listen on
- */
+/* The port that the server will listen on */
 const PORT = 8080;
 
+const cors = require('cors');
+
+
 /* Middleware*/
-/**
- * Configure express to use its own body parsing functionality as middle-ware
+
+
+/* Configure the server's cors policy to only accept requests from servers that have the same host, protocol and port */
+app.use(cors({
+	credentials: 'same-origin'
+}));
+
+/* Configure express to use its own body parsing functionality as the parsing middle-ware,
+ * instead of installing the body-parser package, which's not needed anymore
  */
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
-/**
- * Cors for cross origin allowance
- */
-
-/**
- * Initialize the main project folder
- */
+/* Initialize the main project folder */
 app.use(express.static('website'));
 
-/**
- * Establish route handling
- */
+
+/* Establish route handling */
 
 
 /**
@@ -54,17 +49,19 @@ app.post('/addentry', (req, res) => {
 	/**
 	 * Validate the incoming data before storing it in projectData
 	 */
-	if (incomingData && incomingData.temperature && incomingData.date && incomingData.userRes) {
-		projectData[`${incomingData.date}`] = incomingData;
+	if (incomingData && incomingData.date && incomingData.temp && incomingData.content) {
+		projectData.date = incomingData.date;
+		projectData.temp = incomingData.temp;
+		projectData.content = incomingData.content;
 		res.status(200).json(projectData);
 	} else {
 		/**
-		 * Respond with 422: Unprocessable Entity status code, because the body is syntactically correct but semantically wrong
+		 * Respond with 400: Bad Request status code, because the JSON object is syntactically correct but semantically wrong
 		 */
-		res.status(422).json({'error': 'Your request\'s body should include the temperature, date and user response fields!'});
+		res.status(400).json({'error': 'Your request\'s body should include the temperature, date and user response fields!'});
 	}
 });
-/**
- * Setup Server
- */
+
+
+/* Setup Server */
 app.listen(PORT, () => console.log(`The server is working on localhost:${PORT}`));
